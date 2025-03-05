382689a69b4fbb34d947499b4e2c318c94d8a0a8
Vivek Omkar Machade <vivekmachade323@gmail.com>
2025-03-05 10:38:48 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/spring/addressbook/SpringAddressbookApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.spring.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAddressbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAddressbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/spring/addressbook/SpringAddressbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.spring.addressbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAddressbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
