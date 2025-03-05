577581fd07ed67437298af73a41747ca443f631f
Vivek Omkar Machade <vivekmachade323@gmail.com>
2025-03-05 11:26:40 +0530

    [Vivek Machade] Add . Service Layer Integration

--- src/main/java/com/bridgelabz/spring/addressbook/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/spring/addressbook/service/AddressBookService.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.spring.addressbook.service;
 
+import com.bridgelabz.spring.addressbook.dto.AddressBookDto;
 import com.bridgelabz.spring.addressbook.model.AddressBookEntry;
 import com.bridgelabz.spring.addressbook.repository.AddressBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
 @Service
 public class AddressBookService {
+
     @Autowired
     private AddressBookRepository repository;
 
@@ -21,21 +22,20 @@ public class AddressBookService {
         return repository.findById(id);
     }
 
-    public AddressBookEntry addEntry(AddressBookEntry entry) {
+    public AddressBookEntry addEntry(AddressBookDto dto) {
+        AddressBookEntry entry = new AddressBookEntry(dto);
         return repository.save(entry);
     }
 
-    public AddressBookEntry updateEntry(Long id, AddressBookEntry updatedEntry) {
+    public Optional<AddressBookEntry> updateEntry(Long id, AddressBookDto dto) {
         return repository.findById(id).map(entry -> {
-            entry.setName(updatedEntry.getName());
-            entry.setPhoneNumber(updatedEntry.getPhoneNumber());
+            entry.setName(dto.getName());
+            entry.setPhoneNumber(dto.getPhoneNumber());
             return repository.save(entry);
-        }).orElse(null);
+        });
     }
 
     public void deleteEntry(Long id) {
         repository.deleteById(id);
     }
-
-
 }
